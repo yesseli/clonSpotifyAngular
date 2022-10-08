4397e0f65cdd4228a29fe03c742742b33ecc94c5
Yessenia Mateo <yesselimateo@gmail.com>
2022-10-08 07:37:36 -0600

    Creación de módulos y componentes compartidos

---
+++ src/app/shared/components/card-player/card-player.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
+
+@Component({
+  selector: 'app-card-player',
+  templateUrl: './card-player.component.html',
+  styleUrls: ['./card-player.component.css']
+})
+export class CardPlayerComponent implements OnInit {
+  @Input() mode: 'small' | 'big' = 'small'
+  @Input() track: TrackModel = { _id: 0, name: '', album: '', url: '', cover: '' };
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
+
+@Component({
+  selector: 'app-media-player',
+  templateUrl: './media-player.component.html',
+  styleUrls: ['./media-player.component.css']
+})
+export class MediaPlayerComponent implements OnInit {
+  mockCover: TrackModel = {
+    cover: 'https://i.scdn.co/image/ab67616d0000b27345ca41b0d2352242c7c9d4bc',
+    album: 'Gioli y Assia',
+    name: 'BEBE (Oficial)',
+    url: 'http://localhost/track.mp3',
+    _id: 1
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
